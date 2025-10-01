5cd275d28dbfe6cba6bf4a9286a18e5229923b3a
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-01 15:28:43 +0530

    expressServer

---
+++ expserver.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+//Here position metters when we are using the use middleware
+app.use("/hello", (req, res) => {
+  res.send("Hello from hello tab");
+});
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+const port = process.env.PORT_NO;
+app.listen(port);
+/*  Use npm run <Script name to start this*/

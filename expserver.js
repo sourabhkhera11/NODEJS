1d091f684d068c990cf5943843cd28a6ca9c171d
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-02 14:34:43 +0530

    QueryAndParams

--- expserver.js
+++ expserver.js
@@ -16,6 +16,19 @@ app.post("/user", (req, res) => {
   console.log(data);
   res.send(`Data is received ${data}`);
 });
+//query parameter
+app.post("/rollno", (req, res) => {
+  //Object destructuring on the fly
+  const { rollno: id, name: Name } = req.query;
+  console.log(req.query);
+  res.send(`Rollno: ${id} and Name=${Name}`);
+});
+//Dynamic parameters
+app.get("/student/:id", (req, res) => {
+  const id = req.params;
+  console.log(id);
+  res.send(`Student id =${id.id}`);
+});
 app.use("/", (req, res) => {
   res.send("Hello World");
 });

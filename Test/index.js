da78158b985688cfbed3e853024e4d89c259fd72
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-10 10:12:02 +0530

    Post

---
+++ Test/index.js
@@ -0,0 +1,9 @@
+// "use strict";
+console.log(this);
+
+
+
+
+// (function test() {
+//   console.log(this);
+// })();
---
+++ test.js
@@ -0,0 +1,6 @@
+"use strict";
+// console.log(this);
+
+(function test() {
+  console.log(this);
+})();

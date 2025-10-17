e3a36dea00e1f6cbe24aa4053ec38da519b01dbc
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-17 14:12:30 +0530

    Login page frontend through daizyUI

--- frontend/DevTinder/src/components/Login.jsx
+++ frontend/DevTinder/src/components/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center mt-5">
+      <fieldset className="fieldset bg-base-200 border-base-500 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend text-2xl">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="xyz@gmail.com" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input" placeholder="********" />
+
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;

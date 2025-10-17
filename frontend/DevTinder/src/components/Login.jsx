c6ffa3bb9964e91dd7ca42821c75366567fd29a8
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-17 14:23:37 +0530

    2 Way binding using useState + value & onChange attributes of input fields

--- frontend/DevTinder/src/components/Login.jsx
+++ frontend/DevTinder/src/components/Login.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-
+import { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //To diplay there value on frontend use {email} thats how there value will be displaed 
+  //Here we have done 2 way binding : Which is used to take the control of input fields in the hands of react 
+  //Other wise it will be managed by dom and browser and on the go you cant validate them 
   return (
     <div className="flex justify-center mt-5">
       <fieldset className="fieldset bg-base-200 border-base-500 rounded-box w-xs border p-4">
         <legend className="fieldset-legend text-2xl">Login</legend>
 
         <label className="label">Email</label>
-        <input type="email" className="input" placeholder="xyz@gmail.com" />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="input"
+          placeholder="xyz@gmail.com"
+        />
 
         <label className="label">Password</label>
-        <input type="password" className="input" placeholder="********" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="input"
+          placeholder="********"
+        />
 
         <button className="btn btn-neutral mt-4">Login</button>
       </fieldset>

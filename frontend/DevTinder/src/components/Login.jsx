46b9494b62f48f115f34bbeb21c0fb88752cb492
sourabhkhera11 <sourabhkhera11@gmail.com>
2025-10-17 14:49:20 +0530

    loginHandler + Axios setUp

--- frontend/DevTinder/src/components/Login.jsx
+++ frontend/DevTinder/src/components/Login.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  //To diplay there value on frontend use {email} thats how there value will be displaed 
-  //Here we have done 2 way binding : Which is used to take the control of input fields in the hands of react 
-  //Other wise it will be managed by dom and browser and on the go you cant validate them 
+  //To diplay there value on frontend use {email} thats how there value will be displaed
+  //Here we have done 2 way binding : Which is used to take the control of input fields in the hands of react
+  //Other wise it will be managed by dom and browser and on the go you cant validate them
+  const loginHandler = async () => {
+    try {
+      const data = await axios.post("http://localhost:4000/hospital/login", {
+        email,
+        password,
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <div className="flex justify-center mt-5">
       <fieldset className="fieldset bg-base-200 border-base-500 rounded-box w-xs border p-4">
@@ -29,7 +41,9 @@ const Login = () => {
           placeholder="********"
         />
 
-        <button className="btn btn-neutral mt-4">Login</button>
+        <button className="btn btn-neutral mt-4" onClick={loginHandler}>
+          Login
+        </button>
       </fieldset>
     </div>
   );
